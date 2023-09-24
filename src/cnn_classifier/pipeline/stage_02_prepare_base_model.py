5358e269a89f52d8702867aaa1108fe8d8db673a
u-droid <u.tyagi2013@gmail.com>
2023-09-24 23:14:31 +0530

    Completed dvc

--- src/cnn_classifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnn_classifier/pipeline/stage_02_prepare_base_model.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
+        obj = PrepareBaseModelPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
